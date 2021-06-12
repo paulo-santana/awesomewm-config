5b98872bb5696793037bf2cc848f30dfc26a9090
Paulo Santana <paulo.santana.r@gmail.com>
2021-06-12 19:36:32 -0300

    refactor: change global setups to rc.lua file

--- rc.lua
+++ rc.lua
@@ -1,4 +1,11 @@
+---@diagnostic disable: lowercase-global
 require('settings.bootstrap')
+
+-- This is used later as the default terminal and editor to run.
+terminal = "kitty"
+editor = os.getenv("EDITOR") or "nvim"
+editor_cmd = terminal .. " -e " .. editor
+
 require('settings.layouts')
 require('settings.rules')
 require('settings.clients')
--- settings/bootstrap.lua
+++ settings/bootstrap.lua
@@ -29,9 +29,3 @@ do
     end)
 end
 -- }}}
-
--- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("EDITOR") or "vim"
-editor_cmd = terminal .. " -e " .. editor
-
